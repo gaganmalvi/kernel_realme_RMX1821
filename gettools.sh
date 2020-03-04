dd0a9a67af1edd67153f1ad207fdabe4177db412
Gagan Malvi <malvigagan@gmail.com>
2020-03-04 20:21:48 +0530

    Create gettools.sh

---
+++ gettools.sh
@@ -0,0 +1,82 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: GPL-3.0-or-later
+# Copyright (C) 2018 Raphiel Rollerscaperers (raphielscape)
+# Copyright (C) 2018 Rama Bondan Prakoso (rama982)
+# Android Kernel Build Script
+
+# Main environtment
+KERNEL_DIR=${HOME}/$(basename $(pwd))
+KERN_IMG=$KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb
+ZIP_DIR=$KERNEL_DIR/AnyKernel3
+CONFIG=oppo6771_18601_defconfig
+CROSS_COMPILE="aarch64-linux-android-"
+CROSS_COMPILE_ARM32="arm-linux-androideabi-"
+PATH=:"${KERNEL_DIR}/clang/clang-4691093/bin:${PATH}:${KERNEL_DIR}/stock/bin:${PATH}:${KERNEL_DIR}/stock_32/bin:${PATH}"
+
+# Export
+export ARCH=arm64
+export CROSS_COMPILE
+export CROSS_COMPILE_ARM32
+
+# Install build package for debian based linux
+sudo apt install bc bash git-core gnupg build-essential \
+    zip curl make automake autogen autoconf autotools-dev libtool shtool python \
+    m4 gcc libtool zlib1g-dev flex
+
+# Clone toolchain
+git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 -b android-9.0.0_r45 --depth=1 stock
+git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 -b android-9.0.0_r45 --depth=1 stock_32
+
+# Clone AnyKernel3
+git clone https://github.com/gaganmalvi/AnyKernel3_rmx1821
+
+#Download Clang
+if [ ! -d clang ]; then
+    wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/tags/android-9.0.0_r45/clang-4691093.tar.gz
+    mkdir -p clang/clang-4691093/
+    tar xvzf clang-4691093.tar.gz -C clang/clang-4691093
+    rm clang-4691093.tar.gz
+fi
+
+# Build start
+make $CONFIG
+make -j$(nproc --all) O=out \
+                      ARCH=arm64 \
+                      CC=clang \
+CLANG_TRIPLE=aarch64-linux-gnu- \
+CROSS_COMPILE=aarch64-linux-android-
+if ! [ -a $KERN_IMG ]; then
+    echo "Build error!"
+    exit 1
+fi
+
+cd $ZIP_DIR
+make clean &>/dev/null
+cd ..
+
+# For MIUI Build
+# Credit Adek Maulana <adek@techdro.id>
+OUTDIR="$KERNEL_DIR/out/"
+VENDOR_MODULEDIR="$KERNEL_DIR/AnyKernel3/modules/vendor/lib/modules"
+STRIP="$KERNEL_DIR/stock/bin/$(echo "$(find "$KERNEL_DIR/stock/bin" -type f -name "aarch64-*-gcc")" | awk -F '/' '{print $NF}' |\
+            sed -e 's/gcc/strip/')"
+for MODULES in $(find "${OUTDIR}" -name '*.ko'); do
+    "${STRIP}" --strip-unneeded --strip-debug "${MODULES}"
+    "${OUTDIR}"/scripts/sign-file sha512 \
+            "${OUTDIR}/certs/signing_key.pem" \
+            "${OUTDIR}/certs/signing_key.x509" \
+            "${MODULES}"
+    find "${OUTDIR}" -name '*.ko' -exec cp {} "${VENDOR_MODULEDIR}" \;
+    case ${MODULES} in
+            */wlan.ko)
+        cp "${MODULES}" "${VENDOR_MODULEDIR}/pronto_wlan.ko" ;;
+    esac
+done
+echo -e "\n(i) Done moving modules"
+rm "${VENDOR_MODULEDIR}/wlan.ko"
+cd $ZIP_DIR
+cp $KERN_IMG zImage
+make normal &>/dev/null
+echo "Flashable zip generated under $ZIP_DIR."
+cd ..
+# Build end
